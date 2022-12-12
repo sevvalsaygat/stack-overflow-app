6d9285c19a6747430b87eea8a680ce2ff2ead5e3
Şevval Saygat <65735565+sevvalsaygat@users.noreply.github.com>
2022-12-12 16:05:06 +0300

    Add react router (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 
 function App() {
-  const _a = 1;
-
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path='/' element={<div>Home</div>} />
+      <Route path='/list' element={<div>List</div>} />
+    </Routes>
+  );
 }
 
 export default App;
