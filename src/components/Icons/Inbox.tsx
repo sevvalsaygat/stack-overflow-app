698dcc7b52a8dac16696ca941fc4cd478b1cb082
sevvalsaygat <sevvallsstt@gmail.com>
2022-12-20 20:20:20 +0300

    feat: add icons

--- src/components/Icons/Inbox.tsx
+++ src/components/Icons/Inbox.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const SvgInbox = () => {
+type SvgInboxType = {
+  className?: string;
+};
+
+const SvgInbox = ({ className }: SvgInboxType) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -8,7 +12,8 @@ const SvgInbox = () => {
       viewBox="0 0 24 24"
       strokeWidth="1.5"
       stroke="currentColor"
-      className="w-6 h-6">
+      className={className}
+    >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
@@ -16,7 +21,8 @@ const SvgInbox = () => {
         2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25
         0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0
         0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25
-        0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z" />
+        0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
+      />
     </svg>
   );
 };
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,5 +1,7 @@
 import SvgHelp from './Help';
 import SvgInbox from './Inbox';
 import SvgSnow from './Snow';
+import SvgCup from './Cup';
+import SvgList from './List';
 
-export default { SvgHelp, SvgInbox, SvgSnow };
+export default { SvgHelp, SvgInbox, SvgSnow, SvgCup, SvgList };
