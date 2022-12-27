814ae440536b21da0d0843689fc8055ebe6233df
sevvalsaygat <sevvallsstt@gmail.com>
2022-12-27 18:10:50 +0300

    feat: design header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Icons } from './../index';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="flex flex-row gap-5 bg-light">
+      <Icons.SvgLogo className="h-8 w-40" />
+      <div className="flex flex-row gap-5">
+        <div>Products</div>
+        <input className="border-2" type="text" placeholder="Search..." />
+        <Icons.SvgInbox className="h-5 w-5" />
+        <Icons.SvgCup className="h-5 w-5" />
+        <Icons.SvgHelp className="h-5 w-5" />
+        <Icons.SvgSnow className="h-5 w-5" />
+        <Icons.SvgList className="h-5 w-5" />
+      </div>
+    </div>
   );
 };
 
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -3,5 +3,17 @@ import SvgInbox from './Inbox';
 import SvgSnow from './Snow';
 import SvgCup from './Cup';
 import SvgList from './List';
+import SvgLogo from './Logo';
+import SvgProfil from './Profil';
 
-export default { SvgHelp, SvgInbox, SvgSnow, SvgCup, SvgList };
+/* eslint-disable */
+export default {
+  SvgHelp,
+  SvgInbox,
+  SvgSnow,
+  SvgCup,
+  SvgList,
+  SvgLogo,
+  SvgProfil,
+  /* eslint-enable */
+};
